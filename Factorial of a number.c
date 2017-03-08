24da9ed5f0ab2663fdac1701e93b4ab7cef7f1c5
madhurashinde <smadhura23@gmail.com>
2017-03-08 19:36:59 +0530

    Add files via upload

---
+++ Factorial of a number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+
+{
+	
+	int n,i,f=1;
+	
+	printf("enter number:\n");
+	
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	
+	{
+	  
+		f=f*i;
+	
+	}
+	
+	printf("%d \n",f);
+
+}
