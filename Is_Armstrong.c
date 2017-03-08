91478a64163b1338f330888bd3f3905ab9803e59
madhurashinde <smadhura23@gmail.com>
2017-03-08 22:22:25 +0530

    Add files via upload

---
+++ Is_Armstrong.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main()
+
+{
+	
+	int sum=0,n,b,rem;
+	
+	printf("Enter the number\n");
+	
+	scanf("%d",&n);
+	b=n;
+	
+	while(b!=0)
+	
+	{
+		
+		rem=b%10;
+		
+		sum=sum+(rem*rem*rem);
+		
+		b=b/10;
+	
+	}
+	
+	if(sum==n)
+	
+	{
+		
+	printf("The number is an armstrong\n");
+	
+	}
+	
+	else
+	
+	{
+		
+	printf("The number is not an armstrong\n");
+	
+	}
+
+}
+		
