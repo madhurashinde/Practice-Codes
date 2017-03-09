d227cb733b774061e8028bea509a36033dc9cbba
madhurashinde <smadhura23@gmail.com>
2017-03-09 20:56:58 +0530

    Add files via upload

---
+++ Pattern.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+
+{
+	
+	int i,j,rows;
+	
+	printf("Enter the rows");
+	
+	scanf("%d",&rows);
+	
+	for(i=1;i<=rows;i++)
+	
+	{
+		
+		for(j=1;j<=i;j++)
+		
+	{
+			
+		printf("*");
+		
+	}
+		
+	printf("\n");
+	
+	}
+
+}
